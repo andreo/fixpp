d1951498f4203b942c34589f1907666c087894b2
andreo <andrey.torba@gmail.com>
2013-05-24 23:51:36 +0300

    add include files to stdafx.h

--- quickfix/src/stdafx.h
+++ quickfix/src/stdafx.h
@@ -1,5 +1,10 @@
 
-#include <node.h>
-#include <v8.h>
-#include <string>
+#include <exception>
 #include <iostream>
+#include <node.h>
+#include <quickfix/DataDictionary.h>
+#include <quickfix/FieldMap.h>
+#include <quickfix/Message.h>
+#include <string>
+#include <uv.h>
+#include <v8.h>
