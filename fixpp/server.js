fcb3215eec5db122b09253797ba9281a8e8b9091
andreo <andrey.torba@gmail.com>
2013-07-24 21:26:07 +0300

    server.js

--- fixpp/server.js
+++ fixpp/server.js
@@ -1,9 +1,15 @@
 
+var winston = require('winston');
+var log = winston.loggers.get('server');
 var express = require('express');
 var fs = require('fs');
 var util = require('util');
-var winston = require('winston');
-var logger = winston.loggers.get('fixpp');
+var Rx = require('rx');
+var quickfix = require('quickfix');
+quickfix.extend(Rx);
+var rxutil = require('rxutil');
+rxutil.extend(Rx);
+var fixpp = require('./lib/fixpp');
 
 var httpLogger = express.logger(
     {
@@ -11,14 +17,18 @@ var httpLogger = express.logger(
 	stream: fs.createWriteStream('/Users/andreo/trash/logs/fixpp.log', { flags: 'a' })
     });
 
+var config = require(process.env.CONFIG || __dirname + "/config.json");
+var fixpp = new fixpp.FixPP(Rx, config);
+
 var app = express()
     .use(httpLogger)
-    .use(express.static(__dirname + '/public'));
+    .use(express.static(__dirname + '/public'))
+    .post('/fixpp', fixpp.prettyPrint.bind(fixpp));
 
-var host = 'localhost';
-var port = 1234;
+var host = config.host || 'localhost';
+var port = config.port || 3000;
 
 app.listen(port, host, function () {
     var url = 'http://' + host + ':' + port + '/';
-    logger.info(url);
+    log.info(url);
 });
