c349f9938e45a9f2fdce2b0637679dcf5fcd0aa1
andreo <andrey.torba@gmail.com>
2013-07-24 21:52:29 +0300

    implement fixpp

---
+++ fixpp/lib/fixpp.js
@@ -0,0 +1,24 @@
+
+var util = require('util');
+var log = require('winston').loggers.get('fixpp');
+
+var request = require('./request');
+var specstorage = require('./specstorage');
+var nameresolver = require('./DictionaryNameResolver');
+var quickfix = require('quickfix');
+
+function FixPP (Rx, config) {
+    this.specstorage = specstorage(Rx, config.specpath);
+    this.nameResolver = nameresolver(config.dictionaryMap, 'FIX50SP2.xml');
+    this.parseJSON = JSON.parse;
+    this.createMessage = function () { return new quickfix.Message(); }
+    this.Rx = Rx;
+    // this.request = {};
+}
+
+FixPP.prototype.prettyPrint = function (req, res) {
+    var r = new request.Request(req, res, this);
+    // this.request[request.id] = request;
+};
+
+exports.FixPP = FixPP;
